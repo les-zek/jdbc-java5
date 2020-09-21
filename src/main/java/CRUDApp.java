942d61736021800a258d489b417657d5304e04dd
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-09-21 09:30:08 +0200

    jdbc crud 3

--- src/main/java/CRUDApp.java
+++ src/main/java/CRUDApp.java
@@ -1,17 +1,13 @@
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class CRUDApp {
-
     static Scanner scanner = new Scanner(System.in);
 
     public static int menu() {
         System.out.println("1. Utwórz tabelę");
-        System.out.println("2. Dodaj rekord ");
+        System.out.println("2. Dodaj rekord");
         System.out.println("3. Wyświetl tabelę");
         System.out.println("0. Wyjście");
         while (!scanner.hasNextInt()) {
@@ -22,36 +18,46 @@ public class CRUDApp {
         return option;
     }
 
-    public static void insertRecord() {
-
+    public static void insertRecord() throws IllegalAccessException, InstantiationException, SQLException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
+        System.out.println("Podaj imię:");
+        String name = scanner.nextLine();
+        System.out.println("Podaj punkty:");
+        int points = scanner.nextInt();
+        Connection connection = ConnectionDemo.getConnection();
+        PreparedStatement insert = connection.prepareStatement(
+                "insert into demo(name, points) values(? ,?)"
+        );
+        insert.setString(1, name);
+        insert.setInt(2, points);
+        insert.executeUpdate();
+        connection.close();
     }
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, SQLException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
         while (true) {
             switch (menu()) {
                 case 1:
-                    // zdefiniowac metode tworzenia tabeli
+                    //TODO zdefiniować metode tworzenie własnej tabeli
                     CreateTableStatementDemo.createTableDemo();
                     break;
                 case 2:
-                    System.out.println("Podaj imie");
-                    String name = scanner.nextLine();
-                    System.out.println("Podaj punkty: ");
-                    int points = scanner.nextInt();
-                    Connection connection = ConnectionDemo.getConnection();
-                    PreparedStatement insert = connection.prepareStatement(
-                            "insert into demo(name, points) values (?,?)"
-                    );
-                    insert.setString(1, name);
-                    insert.setInt(2, points);
-                    insert.executeUpdate();
-                    connection.close();
+                    insertRecord();
                     break;
                 case 3:
+                    //wyświetl tabelę
+                    Connection connection = ConnectionDemo.getConnection();
+
+                    Statement selectAll = connection.createStatement(
+                            ResultSet.TYPE_SCROLL_INSENSITIVE,
+                            ResultSet.CONCUR_READ_ONLY);
+                    ResultSet records = selectAll.executeQuery("select * from demo");
+
+                    SelectDemo.printDemoTable(records);
+                    connection.close();
                     break;
                 case 0:
-                    return;
-                //      System.exit(0);
+                    System.exit(0);
+                    //TODO dodać modyfikację, usuwanie i wyszukiwanie rekordów
             }
         }
     }
--- src/main/java/MetaDataResultSetDemo.java
+++ src/main/java/MetaDataResultSetDemo.java
@@ -1,26 +1,14 @@
 import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MetaDataResultSetDemo {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, SQLException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
-
         Connection connection = ConnectionDemo.getConnection();
-
         Statement selectAll = connection.createStatement();
         ResultSet records = selectAll.executeQuery("select * from demo");
         ResultSetMetaData metaData = records.getMetaData();
-        for (int i = 0; i < metaData.getColumnCount(); i++) {
-            System.out.print(metaData.getColumnName(i + 1) + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < metaData.getColumnCount(); i++) {
-            System.out.print(metaData.getColumnClassName(i + 1) + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < metaData.getColumnCount(); i++) {
-            System.out.print(metaData.getColumnType(i + 1) + " ");
-        }
-        System.out.println("--------------------------------------------");
         int validColumns = 0;
         for (int i = 0; i < metaData.getColumnCount(); i++) {
             if (metaData.getColumnName(i + 1).equals("id")) {
@@ -32,9 +20,19 @@ public class MetaDataResultSetDemo {
             if (metaData.getColumnName(i + 1).equals("points")) {
                 validColumns++;
             }
-
         }
+        List<Player> players = new ArrayList<>();
+        if (validColumns == 3) {
+            while (records.next()) {
+                Player player = new Player(
+                        records.getInt("id"),
+                        records.getString("name"),
+                        records.getInt("points")
+                );
+                players.add(player);
+            }
+        }
+        players.forEach(System.out::println);
+
     }
-
-
 }
