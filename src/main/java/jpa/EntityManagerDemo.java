c21d66a95f8de6ea871fab166cec1ff6bfd438a3
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-09-26 11:49:11 +0200

    hibarnate course 2020-09-26 4

--- src/main/java/jpa/EntityManagerDemo.java
+++ src/main/java/jpa/EntityManagerDemo.java
@@ -1,11 +1,53 @@
 package jpa;
 
+import entity.Demo;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 public class EntityManagerDemo {
 
     public static void main(String[] args) {
         EntityManager em = MyPersistence.JAVA5.getEntityManager();
+        // znajdowanie encji w bazie
+        Demo demo = em.find(Demo.class, 1);
+        System.out.println(demo);
+        Demo newDemo = new Demo();
+        newDemo.setName("TESTOWY");
+        newDemo.setPoints(300);
+
+        // dodanie encji do bazy
+        // robimy transakcję
+        em.getTransaction().begin();
+        em.persist(newDemo);
+        em.getTransaction().commit();
+        // koniec transakcji
+
+        Query query = em.createQuery(
+                "select d from Demo d where d.name = :qname and d.points = :qpoints",
+                Demo.class);
+        // d jest alias
+        // gdy chcemy wybrane pole to select d.pole
+        query.setParameter("qname", "TESTOWY");
+        query.setParameter("qpoints", 300);
+        List<Demo> resultList = query.getResultList();
+        for (Demo d : resultList) {
+            System.out.println(d);
+
+        }
+
+        // usuwanie encji z bazy
+        em.getTransaction().begin();
+        em.remove(demo);
+        em.getTransaction().commit();
+        // obiekt usuniety z bazy ale istniejacy w pamieci
+        System.out.println(demo);
+        demo = em.find(Demo.class,1);
+        // obiekt pobrany z bazy po usunieciu
+        System.out.println(demo);
+
+
         em.close();
     }
 }
