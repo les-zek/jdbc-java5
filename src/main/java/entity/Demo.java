54cb0d0a776094a8e7f41b08c0f52a97f033d206
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-09-26 10:50:03 +0200

    hibarnate course 2020-09-26 3

---
+++ src/main/java/entity/Demo.java
@@ -0,0 +1,46 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Demo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="name")  // gdy chcemy kolumne nazwac inaczej niz jest w bazie
+    private String name;
+    private int points;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", points=" + points +
+                '}';
+    }
+}
