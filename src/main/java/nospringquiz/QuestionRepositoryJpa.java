66b2d746135ace8e970ed5b3b91d24e06bd2ffc2
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-09-26 13:02:46 +0200

    hibarnate course 2020-09-26 quiz app 1

---
+++ src/main/java/nospringquiz/QuestionRepositoryJpa.java
@@ -0,0 +1,63 @@
+package nospringquiz;
+
+import entity.Question;
+import jpa.MyPersistence;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class QuestionRepositoryJpa implements QuestionRepository {
+
+    //    private final MyPersistence persistence = MyPersistence.QUIZ;
+    private final MyPersistence persistence;
+
+    public QuestionRepositoryJpa(MyPersistence persistence) {
+        this.persistence = persistence;
+    }
+
+    @Override
+    public void save(Question question) {
+        EntityManager em = persistence.getEntityManager();
+        em.getTransaction().begin();
+        em.persist(question);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public Optional<Question> findById(long id) {
+        EntityManager em = persistence.getEntityManager();
+        Optional<Question> question = Optional.ofNullable(em.find(Question.class, id));
+        em.close();
+        return question;
+    }
+
+    @Override
+    public void delete(Question question) {
+        EntityManager em = persistence.getEntityManager();
+        em.getTransaction().begin();
+        em.remove(question);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void update(long id, Question question) {
+        EntityManager em = persistence.getEntityManager();
+        em.getTransaction().begin();
+        Question entity = em.find(Question.class, id);
+        if (entity == null) {
+            em.close();
+            em.getTransaction().commit();
+            return;
+        }
+        entity.setBody(question.getBody());
+        
+    }
+
+    @Override
+    public List<Question> findAll() {
+        return null;
+    }
+}
